dd87723fbb21efe4d9dcef8eb3451ffce2aae8d3
OscarNZ13 <onaranjo00330@ufide.ac.cr>
2024-03-18 11:17:44 -0600

    Registro de usuarios completo

--- Model/User_Model.php
+++ Model/User_Model.php
@@ -5,53 +5,45 @@ class UserModel
 {
     public function authenticateUser($username, $password, $conn)
     {
-        // Inicializar $auth_status
-        $auth_status = 0;
-
-        // Llama al procedimiento almacenado
-        $sql = "BEGIN Authenticate_User(:username, :password, :auth_status); END;";
+        // Obtener el hash de la contraseña almacenada en la base de datos
+        $sql = "SELECT contrasena FROM Usuarios WHERE nombre_usuario = :username";
         $stmt = oci_parse($conn, $sql);
-
-        // Bind de parámetros
         oci_bind_by_name($stmt, ':username', $username);
-        oci_bind_by_name($stmt, ':password', $password);
-        oci_bind_by_name($stmt, ':auth_status', $auth_status, 1); // Tipo de dato OUT
-
-        // Ejecutar el procedimiento almacenado
         oci_execute($stmt);
+        
+        $row = oci_fetch_assoc($stmt);
+        $stored_hash = $row['CONTRASENA'];
 
-        // Verificar el resultado
-        if ($auth_status == 1) {
+        // Verificar si el usuario existe y la contraseña es correcta
+        if ($stored_hash && password_verify($password, $stored_hash)) {
             return true; // Autenticación exitosa
         } else {
             return false; // Autenticación fallida
         }
     }
 
-
-    public function RegisterUser($username, $email, $password, $location)
+    public function RegisterUser($username, $password, $conn)
     {
+        //Convertir la contraseña en un hash
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        // Convertir la contraseña en un hash seguro
+        //Llama al procedimiento almacenado
+        $sql = "BEGIN InsertarUsuario(:username, :hashed_password); END;";
+        $stmt = oci_parse($conn, $sql);
 
-        // Verificar si la consulta se ejecutó correctamente
+        //Bind de parámetros
+        oci_bind_by_name($stmt, ':username', $username);
+        oci_bind_by_name($stmt, ':hashed_password', $hashed_password);
 
+        //Ejecutar el procedimiento almacenado
+        oci_execute($stmt);
 
-        // Verificar si se encontraron filas en el resultado
-
-    }
-
-    public function ValidarDatos($username, $email, $password, $location)
-    {
-        // Verificar si algún campo está vacío
-
-        // Verificar si el correo electrónico tiene el formato correcto
-
-
-        // Verificar si la contraseña tiene al menos 8 caracteres y contiene al menos un número y una letra
-
-
-        // Si todas las validaciones pasan, devuelve true
-
+        //Verificar si la consulta se ejecutó correctamente
+        $rowsAffected = oci_num_rows($stmt);
+        if ($rowsAffected > 0) {
+            return true; // Registro exitoso
+        } else {
+            return false; // Registro fallido
+        }
     }
 }
