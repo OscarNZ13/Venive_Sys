2d262bb9343f7843aa08f39687f58813d4b387ad
AndresALPD <andresalpizar03@gmail.com>
2024-03-18 19:01:54 -0600

    Procedimientos de Productos

--- DB_Venive.sql
+++ DB_Venive.sql
@@ -43,8 +43,8 @@ CREATE TABLE Inventario (
     id_talla NUMBER,
     cantidad_disponible NUMBER,
     PRIMARY KEY (id_producto, id_talla), -- Clave primaria compuesta
-    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE, -- Restricción ON DELETE CASCADE
-    FOREIGN KEY (id_talla) REFERENCES Tallas(id_talla) -- Clave foránea para la tabla Tallas
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE, -- Restricciï¿½n ON DELETE CASCADE
+    FOREIGN KEY (id_talla) REFERENCES Tallas(id_talla) -- Clave forï¿½nea para la tabla Tallas
 );
 
 DROP TABLE Inventario;
@@ -54,7 +54,7 @@ CREATE TABLE Productos_Categorias (
     id_categoria NUMBER,
     id_producto NUMBER,
     PRIMARY KEY (id_categoria, id_producto), -- Clave primaria compuesta
-    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE, -- Restricción ON DELETE CASCADE
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE, -- Restricciï¿½n ON DELETE CASCADE
     FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria)
 );
 
@@ -65,20 +65,20 @@ CREATE TABLE Productos_Sexo (
     id_sexo NUMBER,
     id_producto NUMBER,
     PRIMARY KEY (id_sexo, id_producto), -- Clave primaria compuesta
-    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE, -- Restricción ON DELETE CASCADE
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE, -- Restricciï¿½n ON DELETE CASCADE
     FOREIGN KEY (id_sexo) REFERENCES Sexo(id_sexo)
 );
 
 DROP TABLE Productos_Sexo;
 
--- Creación de secuencia para el id_usuario en la tabla Usuarios
+-- Creaciï¿½n de secuencia para el id_usuario en la tabla Usuarios
 CREATE SEQUENCE seq_id_usuario
     START WITH 1
     INCREMENT BY 1
     NOCACHE
     NOMAXVALUE;
 
--- Creación del trigger para asignar el id_usuario automáticamente en la tabla Usuarios
+-- Creaciï¿½n del trigger para asignar el id_usuario automï¿½ticamente en la tabla Usuarios
 CREATE OR REPLACE TRIGGER tr_id_usuario
 BEFORE INSERT ON Usuarios
 FOR EACH ROW
@@ -87,14 +87,14 @@ BEGIN
 END;
 /
 
--- Creación de secuencia para el id_sexo en la tabla Sexo
+-- Creaciï¿½n de secuencia para el id_sexo en la tabla Sexo
 CREATE SEQUENCE seq_id_sexo
     START WITH 1
     INCREMENT BY 1
     NOCACHE
     NOMAXVALUE;
 
--- Creación del trigger para asignar el id_sexo automáticamente en la tabla Sexo
+-- Creaciï¿½n del trigger para asignar el id_sexo automï¿½ticamente en la tabla Sexo
 CREATE OR REPLACE TRIGGER tr_id_sexo
 BEFORE INSERT ON Sexo
 FOR EACH ROW
@@ -103,14 +103,14 @@ BEGIN
 END;
 /
 
--- Creación de secuencia para el id_categoria en la tabla Categorias
+-- Creaciï¿½n de secuencia para el id_categoria en la tabla Categorias
 CREATE SEQUENCE seq_id_categoria
     START WITH 1
     INCREMENT BY 1
     NOCACHE
     NOMAXVALUE;
 
--- Creación del trigger para asignar el id_categoria automáticamente en la tabla Categorias
+-- Creaciï¿½n del trigger para asignar el id_categoria automï¿½ticamente en la tabla Categorias
 CREATE OR REPLACE TRIGGER tr_id_categoria
 BEFORE INSERT ON Categorias
 FOR EACH ROW
@@ -119,14 +119,14 @@ BEGIN
 END;
 /
 
--- Creación de secuencia para el id_talla en la tabla Tallas
+-- Creaciï¿½n de secuencia para el id_talla en la tabla Tallas
 CREATE SEQUENCE seq_id_talla
     START WITH 1
     INCREMENT BY 1
     NOCACHE
     NOMAXVALUE;
 
--- Creación del trigger para asignar el id_talla automáticamente en la tabla Tallas
+-- Creaciï¿½n del trigger para asignar el id_talla automï¿½ticamente en la tabla Tallas
 CREATE OR REPLACE TRIGGER tr_id_talla
 BEFORE INSERT ON Tallas
 FOR EACH ROW
@@ -135,14 +135,14 @@ BEGIN
 END;
 /
 
--- Creación de secuencia para el id_producto en la tabla Productos
+-- Creaciï¿½n de secuencia para el id_producto en la tabla Productos
 CREATE SEQUENCE seq_id_producto
     START WITH 1
     INCREMENT BY 1
     NOCACHE
     NOMAXVALUE;
 
--- Creación del trigger para asignar el id_producto automáticamente en la tabla Productos
+-- Creaciï¿½n del trigger para asignar el id_producto automï¿½ticamente en la tabla Productos
 CREATE OR REPLACE TRIGGER tr_id_producto
 BEFORE INSERT ON Productos
 FOR EACH ROW
@@ -230,19 +230,19 @@ IS
     v_nombre_usuario Usuarios.nombre_usuario%TYPE;
     v_rol Usuarios.rol%TYPE;
 BEGIN
-    -- Seleccionar la información del usuario
+    -- Seleccionar la informaciï¿½n del usuario
     SELECT id_usuario, nombre_usuario, rol
     INTO v_id_usuario, v_nombre_usuario, v_rol
     FROM Usuarios
     WHERE id_usuario = p_id_usuario;
     
-    -- Mostrar la información del usuario
+    -- Mostrar la informaciï¿½n del usuario
     DBMS_OUTPUT.PUT_LINE('ID de Usuario: ' || v_id_usuario);
     DBMS_OUTPUT.PUT_LINE('Nombre de Usuario: ' || v_nombre_usuario);
     DBMS_OUTPUT.PUT_LINE('Rol: ' || v_rol);
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No se encontró un usuario con el ID especificado.');
+        DBMS_OUTPUT.PUT_LINE('No se encontrï¿½ un usuario con el ID especificado.');
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Error al mostrar el usuario: ' || SQLERRM);
 END;
@@ -262,16 +262,123 @@ CREATE OR REPLACE PROCEDURE Authenticate_User (
 AS
     v_count NUMBER;
 BEGIN
-    -- Verificar si las credenciales son válidas
+    -- Verificar si las credenciales son vï¿½lidas
     SELECT COUNT(*) INTO v_count
     FROM Usuarios
     WHERE nombre_usuario = p_username AND contrasena = p_password;
     
-    -- Asignar 1 si las credenciales son válidas, 0 en caso contrario
+    -- Asignar 1 si las credenciales son vï¿½lidas, 0 en caso contrario
     IF v_count > 0 THEN
-        p_auth_status := 1; -- Autenticación exitosa
+        p_auth_status := 1; -- Autenticaciï¿½n exitosa
     ELSE
-        p_auth_status := 0; -- Autenticación fallida
+        p_auth_status := 0; -- Autenticaciï¿½n fallida
     END IF;
 END;
 /
+
+-- Crear nueva prenda
+CREATE OR REPLACE PROCEDURE InsertarPrenda (
+    p_nombre_producto IN VARCHAR2,
+    p_precio_compra IN NUMBER,
+    p_precio_venta IN NUMBER,
+    p_porcentaje_ganancia IN NUMBER,
+    p_imagen IN VARCHAR2
+)
+IS
+BEGIN
+    INSERT INTO Productos (nombre_producto, precio_compra, precio_venta, porcentaje_ganancia, imagen)
+    VALUES (p_nombre_producto, p_precio_compra, p_precio_venta, p_porcentaje_ganancia, p_imagen);
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Prenda insertada correctamente.');
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error al insertar prenda: ' || SQLERRM);
+END;
+/
+
+--Ejecutamos
+EXEC InsertarPrenda('Camisa', 20, 40, 100, 'imagen_cami.jpg');
+
+-- Eliminar prenda
+CREATE OR REPLACE PROCEDURE EliminarPrenda (
+    p_id_producto IN NUMBER
+)
+IS
+BEGIN
+    DELETE FROM Productos WHERE id_producto = p_id_producto;
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Prenda eliminada correctamente.');
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error al eliminar prenda: ' || SQLERRM);
+END;
+/
+
+--Ejecutamos
+EXEC EliminarPrenda(1);
+
+-- Modificar prenda
+CREATE OR REPLACE PROCEDURE ModificarPrenda (
+    p_id_producto IN NUMBER,
+    p_nombre_producto IN VARCHAR2 := NULL,
+    p_precio_compra IN NUMBER := NULL,
+    p_precio_venta IN NUMBER := NULL,
+    p_porcentaje_ganancia IN NUMBER := NULL,
+    p_imagen IN VARCHAR2 := NULL
+)
+IS
+BEGIN
+    UPDATE Productos
+    SET nombre_producto = COALESCE(p_nombre_producto, nombre_producto),
+        precio_compra = COALESCE(p_precio_compra, precio_compra),
+        precio_venta = COALESCE(p_precio_venta, precio_venta),
+        porcentaje_ganancia = COALESCE(p_porcentaje_ganancia, porcentaje_ganancia),
+        imagen = COALESCE(p_imagen, imagen)
+    WHERE id_producto = p_id_producto;
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Prenda modificada correctamente.');
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error al modificar prenda: ' || SQLERRM);
+END;
+/
+
+--Ejecutamos
+EXEC ModificarPrenda(1, 'Nuevo nombre', NULL, 50, NULL, NULL);
+
+-- Mostrar prenda
+CREATE OR REPLACE PROCEDURE MostrarPrenda (
+    p_id_producto IN NUMBER
+)
+IS
+    v_id_producto Productos.id_producto%TYPE;
+    v_nombre_producto Productos.nombre_producto%TYPE;
+    v_precio_compra Productos.precio_compra%TYPE;
+    v_precio_venta Productos.precio_venta%TYPE;
+    v_porcentaje_ganancia Productos.porcentaje_ganancia%TYPE;
+    v_imagen Productos.imagen%TYPE;
+BEGIN
+    SELECT id_producto, nombre_producto, precio_compra, precio_venta, porcentaje_ganancia, imagen
+    INTO v_id_producto, v_nombre_producto, v_precio_compra, v_precio_venta, v_porcentaje_ganancia, v_imagen
+    FROM Productos
+    WHERE id_producto = p_id_producto;
+    
+    DBMS_OUTPUT.PUT_LINE('ID Producto: ' || v_id_producto);
+    DBMS_OUTPUT.PUT_LINE('Nombre Producto: ' || v_nombre_producto);
+    DBMS_OUTPUT.PUT_LINE('Precio Compra: ' || v_precio_compra);
+    DBMS_OUTPUT.PUT_LINE('Precio Venta: ' || v_precio_venta);
+    DBMS_OUTPUT.PUT_LINE('Porcentaje Ganancia: ' || v_porcentaje_ganancia);
+    DBMS_OUTPUT.PUT_LINE('Imagen: ' || v_imagen);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontrÃ³ una prenda con el ID especificado.');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error al mostrar la prenda: ' || SQLERRM);
+END;
+/
+
+--Ejecutamos
+EXEC MostrarPrenda(1);
